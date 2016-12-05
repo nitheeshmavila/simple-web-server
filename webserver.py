bea9f8fb1851caaaf3e58f661fe6432e17ec8823
nitheeshmavila <mail2nitheeshmavila@gmail.com>
2016-12-05 22:56:16 +0530

    can handle concurrent connections

--- webserver.py
+++ webserver.py
@@ -1,15 +1,19 @@
 import socket
 import sys
+import os
 import time
 
 class HttpServer:
 	
 	def __init__(self, port=8080):
+		''' http server constructor'''	
 		self.port = port
 		self.host_dir = '.' 
 		self.ip_address = '127.0.0.1'
+		self.queue_size = 10
 
 	def create_socket(self):
+		''' this method create a server socket'''
 		self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		try:
 			self.server.bind((self.ip_address, self.port))
@@ -24,41 +28,57 @@ class HttpServer:
 		self.listen()
 
 	def listen(self):
+		''' this method will listen for new connection '''
 		while True:
 			print("Waiting for new connection\n")
-			self.server.listen(1)
-			conn_socket, address =  self.server.accept()
-			requested_data = conn_socket.recv(1024)
-			print("Recived connection from:",address)
-			requested_string = bytes.decode(requested_data)
-			requested_method = requested_string.split(' ')[0]
-			print("Request method:",requested_method)
-	#		print("Request content:\n",requested_string)
-			if(requested_method == 'GET'):
-				requested_file = requested_string.split(' ')[1]
-				if(requested_file == '/'):
-					requested_file = '/index.html'
-				requested_file = self.host_dir + requested_file
-				try:
-					fp = open(requested_file, 'rb')
-					response_data = fp.read()
-					fp.close()
-		#			print("response content:",response_data)
-					content_type = self.get_content_type(requested_file)
-					header = self.make_header(200, content_type)
-				except:
-					header = self.make_header(400, 'text/html')
-					response_data = "<html><body><p> Error 404 File not found</p></body></html>".encode()	
-				final_response = header.encode()
-				final_response += response_data
-				print("Response:",final_response)
-				conn_socket.send(final_response)				  
-				conn_socket.close()
+			self.server.listen(self.queue_size)
+			print('Parent PID : {pid}\n'.format(pid=os.getpid()))
+			client_connection, address =  self.server.accept()
+			pid = os.fork()
+			if(pid == 0):
+				self.server.close()
+				self.handle_request(client_connection, address)
+				client_connection.close()
+				os.exit(0)
 			else:
-				print(requested_method)
-				print("HTTP request method unknown")			
+				client_connection.close()
+
+	def handle_request(self, client_connection, address):
+		'''this method take the request from the client, 
+			process the request and produce the http response to the client''' 
+		requested_data = client_connection.recv(1024)
+		print("Recived connection from:",address)
+		requested_string = bytes.decode(requested_data)
+		requested_method = requested_string.split(' ')[0]
+		print("Request method:",requested_method)
+		print("Request content:\n",requested_string)
+		if(requested_method == 'GET'):
+			requested_file = requested_string.split(' ')[1]
+			if(requested_file == '/'):
+				requested_file = '/index.html'
+			requested_file = self.host_dir + requested_file
+			try:
+				fp = open(requested_file, 'rb')
+				response_data = fp.read()
+				fp.close()
+				content_type = self.get_content_type(requested_file)
+				header = self.make_header(200, content_type)
+			except:
+				header = self.make_header(404, 'text/html')
+				response_data = b'<html><body><p> Error 404 File not found</p></body></html>'
+			final_response = header.encode()
+			final_response += response_data
+			print("Serving:",requested_file)
+			client_connection.send(final_response)				  
+			time.sleep(60)
+			
+		else:
+			print(requested_method)
+			print("HTTP request method unknown")			
 			
 	def make_header(self, http_code, content_type):
+		''' this method takes the http code and content type 
+			and return the header for the response'''
 		headr = ''
 		if(http_code == 200):
 			headr = 'HTTP/1.1 200 OK\n'
@@ -72,11 +92,15 @@ class HttpServer:
 		return headr
 
 	def get_content_type(self, file_name):
+		''' this method extract the file type from the file name
+			and returns the content type that has to included in the header'''
 		content_types = {'html': 'text/html',
 						'txt': 'text/txt',
 						'jpg': 'image/jpeg',
 						'png': 'image/png',
-						'ico': 'icon/ico'}
+						'ico': 'icon/ico',
+						'pdf': 'application/pdf',
+						'gif': 'image/gif'}
 		return content_types[file_name.split('.')[-1]]
 			
 
